95de5b4173fe3ea857ed7233b7d8e089a5876b00
vikash979 <vikash.kumar979@gmail.com>
2020-12-04 21:00:46 +0530

    php project added

---
+++ testdemo/loginsystem/admin/manage-users.php
@@ -0,0 +1,274 @@
+<?php
+session_start();
+include'dbconnection.php';
+// checking session is valid for not 
+if (strlen($_SESSION['id']==0)) {
+  header('location:logout.php');
+  } else{
+
+// for deleting user
+if(isset($_GET['id']))
+{
+$adminid=$_GET['id'];
+$msg=mysqli_query($con,"delete from users where id='$adminid'");
+if($msg)
+{
+echo "<script>alert('Data deleted');</script>";
+}
+}
+
+if(isset($_POST['Submit']))
+{
+  echo '<pre>';
+  print_r($_POST);
+  echo '</pre>';
+  $fname=$_POST['fname'];
+  $lname=$_POST['lname'];
+  $contact=$_POST['contact'];
+  $email=$_POST['email'];
+  
+$query=mysqli_query($con,"insert into  users ( fname ,lname , contactno , email) values('$fname','$lname','$contact','$email') ");
+$_SESSION['msg']="Profile Updated successfully";
+}
+?>
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Dashboard">
+    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+
+    <title>Admin | Manage Users</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/style-responsive.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+  <section id="container" >
+      <header class="header black-bg">
+              <div class="sidebar-toggle-box">
+                  <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Toggle Navigation"></div>
+              </div>
+            <a href="#" class="logo"><b>Admin Dashboard</b></a>
+            <div class="nav notify-row" id="top_menu">
+               
+                         
+                   
+                </ul>
+            </div>
+            <div class="top-menu">
+            	<ul class="nav pull-right top-menu">
+                    <li><a class="logout" href="logout.php">Logout</a></li>
+            	</ul>
+            </div>
+        </header>
+      <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <ul class="sidebar-menu" id="nav-accordion">
+              
+              	  <p class="centered"><a href="#"><img src="assets/img/ui-sam.jpg" class="img-circle" width="60"></a></p>
+              	  <h5 class="centered"><?php echo $_SESSION['login'];?></h5>
+              	  	
+              
+                  <li class="sub-menu">
+                      <a href="manage-users.php" >
+                          <i class="fa fa-users"></i>
+                          <span>Manage Users</span>
+                      </a>
+                   
+                  </li>
+              
+                 
+              </ul>
+          </div>
+      </aside>
+      <section id="main-content">
+          <section class="wrapper">
+          	<h3><i class="fa fa-angle-right"></i> Manage Users</h3>
+				<div class="row">
+				
+                  
+	                  
+                  <div class="col-md-12">
+                      <div class="content-panel">
+                          <table class="table table-striped table-advance table-hover">
+	                  	  	  <h4><i class="fa fa-angle-right"></i> All User Details </h4>
+                            <style>
+body {font-family: Arial, Helvetica, sans-serif;}
+
+/* The Modal (background) */
+.modal {
+  display: none; /* Hidden by default */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Sit on top */
+  padding-top: 100px; /* Location of the box */
+  left: 0;
+  top: 0;
+  width: 100%; /* Full width */
+  height: 100%; /* Full height */
+  overflow: auto; /* Enable scroll if needed */
+  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
+}
+
+/* Modal Content */
+.modal-content {
+  background-color: #fefefe;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%;
+}
+
+/* The Close Button */
+.close {
+  color: #aaaaaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: #000;
+  text-decoration: none;
+  cursor: pointer;
+}
+</style>
+
+
+
+<!-- Trigger/Open The Modal -->
+<button id="myBtn">Open Modal</button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+     <form class="form-horizontal style-form" name="form1" method="post" action="" onSubmit="return valid();">
+                           <p style="color:#F00"></p>
+                          <div class="form-group">
+                              <label class="col-sm-2 col-sm-2 control-label" style="padding-left:40px;">First Name </label>
+                              <div class="col-sm-10">
+                                  <input type="text" class="form-control" name="fname" value="" >
+                              </div>
+                          </div>
+                          
+                              <div class="form-group">
+                              <label class="col-sm-2 col-sm-2 control-label" style="padding-left:40px;">Last Ename</label>
+                              <div class="col-sm-10">
+                                  <input type="text" class="form-control" name="lname" value="" >
+                              </div>
+                          </div>
+                          
+                              <div class="form-group">
+                              <label class="col-sm-2 col-sm-2 control-label" style="padding-left:40px;">Email </label>
+                              <div class="col-sm-10">
+                                  <input type="text" class="form-control" name="email" value=""  >
+                              </div>
+                          </div>
+                               <div class="form-group">
+                              <label class="col-sm-2 col-sm-2 control-label" style="padding-left:40px;">Contact no. </label>
+                              <div class="col-sm-10">
+                                  <input type="text" class="form-control" name="contact" value="" >
+                              </div>
+                          </div>
+                            
+                          <div style="margin-left:100px;">
+                          <input type="submit" name="Submit" value="Update" class="btn btn-theme"></div>
+                          </form>
+  </div>
+
+</div>
+
+<script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+	                  	  	  <hr>
+                              <thead>
+                              <tr>
+                                  <th>Sno.</th>
+                                  <th class="hidden-phone">First Name</th>
+                                  <th> Last Name</th>
+                                  <th> Email Id</th>
+                                  <th>Contact no.</th>
+                                  <th>EDIT /Delete</th>
+                              </tr>
+                              </thead>
+                              <tbody>
+                              <?php $ret=mysqli_query($con,"select * from users");
+							  $cnt=1;
+							  while($row=mysqli_fetch_array($ret))
+							  {?>
+                              <tr>
+                              <td><?php echo $cnt;?></td>
+                                  <td><?php echo $row['fname'];?></td>
+                                  <td><?php echo $row['lname'];?></td>
+                                  <td><?php echo $row['email'];?></td>
+                                  <td><?php echo $row['contactno'];?></td>  
+                                  <
+                                  <td>
+                                     
+                                     <a href="update-profile.php?uid=<?php echo $row['id'];?>"> 
+                                     <button class="btn btn-primary btn-xs"><i class="fa fa-pencil"></i></button></a>
+                                     <a href="manage-users.php?id=<?php echo $row['id'];?>"> 
+                                     <button class="btn btn-danger btn-xs" onClick="return confirm('Do you really want to delete');"><i class="fa fa-trash-o "></i></button></a>
+                                  </td>
+                              </tr>
+                              <?php $cnt=$cnt+1; }?>
+                             
+                              </tbody>
+                          </table>
+                      </div>
+                  </div>
+              </div>
+		</section>
+      </section
+  ></section>
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
+    <script src="assets/js/jquery.scrollTo.min.js"></script>
+    <script src="assets/js/jquery.nicescroll.js" type="text/javascript"></script>
+    <script src="assets/js/common-scripts.js"></script>
+  <script>
+      $(function(){
+          $('select.styled').customSelect();
+      });
+
+  </script>
+
+  </body>
+</html>
+<?php } ?>
